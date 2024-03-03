060e59ed0bf6c932b84755e135356a0f4cf6475e
smartcraze <smartcraze>
2024-03-03 11:09:00 +0530

    question done

---
+++ questions/ca1quest.c
@@ -0,0 +1,28 @@
+// parna ka ca 1 ka question 
+#include <stdio.h>
+void checkingCustomer(int expend){
+    if (expend >=10000)
+    {
+        // premium customer 
+        // giving 10% off
+        int  discount = expend * 0.1;
+        printf("You are Our premium Customer");
+        printf("Your final product after discount is : %d",expend - discount - 200);
+    }
+    else{
+        // regular customer
+        // giving 5% off
+        int discount =expend*0.05 ;
+        printf("You are Our regular Customer");
+        printf("Your final product after discount is : %d",expend - discount - 100);
+        
+    }
+    
+}
+int main() {
+    int expend;
+    printf("Enter the number: ");
+    scanf("%d", &expend);
+    checkingCustomer(expend);
+    return 0;
+}
