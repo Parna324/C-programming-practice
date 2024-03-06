38bbc02aef7c544191abce544e55ca408f70f133
smartcraze <smartcraze>
2024-03-06 15:58:25 +0530

    added 1 question

---
+++ questions/testingtab9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// TODO: remove this
+// FIXME: remove
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    int i = 0;
+    while (i < 10) {
+        printf("Hello, World!\n");
+        i++;
+    }  
+    return 0;
+}
